9739010bfb6bb1c866891cc8cf82e4905b05b935
Wyjbuss <wyjbuss@gmail.com>
2021-04-22 11:45:09 -0500

    bootstrap works, added cards w/ images

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import CardDeck from 'react-bootstrap/CardDeck'
+import {Container,Row,Col} from 'react-bootstrap'
+
+
 
 function Home() {
   return (
@@ -17,14 +23,88 @@ function Home() {
         <p className={styles.description}>
           Getting to know next.js. Making a store front with these cards here.<br />
           Imported bootstrap and react-bootstrap because I plan on using alot of the styles.
-          </p>
+        </p>
+        <br />
           
         
-
+       
         {/* grid with cards go here
         
         grid card card
          */}
+        <Container>
+        <Row style={{justifyContent:'center'}}>
+        <Col sm={12} md={6} lg={4} xl={3} style={{margin: '1rem'}}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img style={{height: '16rem'}} variant="top" src="/images/imgOne.jpg" />
+          <Card.Body>
+            <Card.Title>Grape Vine</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+
+        <Col sm={12} md={6} lg={4} xl={3} style={{margin: '1rem'}}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img style={{height: '16rem'}} variant="top" src="/images/imgThree.jpg" />
+          <Card.Body>
+            <Card.Title>Vineyard</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+
+        <Col sm={12} md={6} lg={4} xl={3} style={{margin: '1rem'}}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img style={{height: '16rem'}} variant="top" src="/images/imgTwo.jpg" />
+          <Card.Body>
+            <Card.Title>Jar of honey</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+
+        <Col sm={12} md={6} lg={4} xl={3} style={{margin: '1rem'}}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img style={{height: '16rem'}} variant="top" src="/images/imgTwo.jpg" />
+          <Card.Body>
+            <Card.Title>Jar of honey</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+
+        <Col sm={12} md={6} lg={4} xl={3} style={{margin: '1rem'}}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img style={{height: '16rem'}} variant="top" src="/images/imgTwo.jpg" />
+          <Card.Body>
+            <Card.Title>Jar of honey</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+        </Row>
+        </Container>
       </main>
 
       <footer className={styles.footer}>
