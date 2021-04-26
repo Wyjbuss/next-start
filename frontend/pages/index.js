32d9277e965098b0e1c4f8b4b19338dbdc65a498
Wyjbuss <wyjbuss@gmail.com>
2021-04-26 11:13:29 -0500

    added information to footer about myself

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -108,14 +108,12 @@ function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Deployed to{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+       <p>
+       Deployed to<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /> By Wyatt Bussell 
+       and projects at <a href="https://github.com/Wyjbuss?tab=repositories">My Github</a>
+       
+       </p> <br />
+        
       </footer>
     </div>
   )
